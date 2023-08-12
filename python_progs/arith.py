55c84c75d69a9a3dfb66e48743c1d49e08f5ac61
vigneshholla <vigneshrao004@gmail.com>
2023-08-12 12:48:10 +0530

    2

--- python_progs/arith.py
+++ python_progs/arith.py
@@ -4,3 +4,10 @@ def add(n1,n2):
 a=int(input("Enter num1="))
 b=int(input("Enter num2="))
 print('sum=',add(a,b))
+
+def sub(n1,n2):
+    return(n1-n2)
+
+a=int(input("Enter num1="))
+b=int(input("Enter num2="))
+print('sum=',sub(a,b))
